a04990799d6740bb5647ef11a7d48cd61036dcca
Natalie1339 <natalieetruesdell@gmail.com>
2025-12-06 22:26:15 +0000

    checking scripts
    fixed loading in of data in data_processing

--- scripts/data_processing.py
+++ scripts/data_processing.py
@@ -37,7 +37,7 @@ def main(raw_data, data_to, preprocessor_to, seed):
                      'alcohol',
                      'quality']
   
-    origin_df = pd.read_csv(raw_data, sep=',', encoding='utf-8')
+    origin_df = pd.read_csv(os.path.join(raw_data, "winequality-white.csv"), sep=',', encoding='utf-8')
 
     schema = pb.Schema(columns=[('fixed_acidity', 'float64'),
                                 ('volatile_acidity', 'float64'),
