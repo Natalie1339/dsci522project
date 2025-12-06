bc0ac90694475b824bc045cd272454114171795e
SamLokanc <SamL.5@icloud.com>
2025-12-05 20:35:12 -0800

    editted option to be consistent with example

--- scripts/data_download.py
+++ scripts/data_download.py
@@ -1,16 +1,17 @@
 import pandas
 import click
+import os
 from ucimlrepo import fetch_ucirepo 
 
 @click.command()
-@click.option('--path', type=str)
-def main(path):
+@click.option('--write-to', type=str)
+def main(write_to):
     wine_quality = fetch_ucirepo(id=186) 
 
     wine_df = wine_quality.data.original
     white_wine_df = wine_df[wine_df['color'] == 'white']
 
-    white_wine_df.to_csv(path)
+    white_wine_df.to_csv(os.path.join(write_to, "winequality-white.csv"))
 
 if __name__ == '__main__':
     main()
